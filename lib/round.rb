5cb874a45a9a0c0a501468f8fab9ce4a40549ada
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-07-07 20:37:51 -0600

    removed psuedocode

--- lib/round.rb
+++ lib/round.rb
@@ -40,33 +40,3 @@ class Round
   def increment_guess
     @current_guess += 1
   end
-
-  def start
-    out_file = File.new("Results - #{Time.new}.txt", "w")
-    puts "Welcome! You're playing with #{@total_cards} cards."
-    deck.cards.each do |card|
-      puts "--------------------------------------------"
-      puts "This is card number #{@current_card + 1} out of #{@total_cards}."
-      puts card.question
-      response = gets.chomp
-      if response.upcase == "HINT"
-        puts "Here's a hint: " + deck.cards[@current_card].hint
-        response = gets.chomp
-      end
-      record_guess(response)
-      move_to_next_card
-      puts @guesses[@current_guess].feedback
-      if @guesses[@current_guess].feedback == "Incorrect."
-        puts "For extra practice we are adding this card to the back of the deck"
-      end
-      out_file.puts("Card Question:  #{card.question}, Card Answer: #{card.answer}, User Response:  #{response}, #{@guesses[@current_guess].feedback}")
-      increment_guess
-      percent_correct
-      sleep(1)
-    end
-    puts "****** Game over! ******"
-    puts "You had #{@number_correct.to_i} guesses out of #{@total_cards} for a score of #{@percent_correct}%"
-    out_file.puts("You had #{@number_correct.to_i} guesses out of #{@total_cards} for a score of #{@percent_correct}%")
-    out_file.close
-  end
-end
